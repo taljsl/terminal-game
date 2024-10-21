410dcb62243aae7791c9fdc780632571c77ab876
Josh L <joshua.levine42@gmail.com>
2024-10-21 19:46:33 -0400

    Cleaned up notes to improve descriptiveness and readability

--- app.js
+++ app.js
@@ -1,56 +1,50 @@
-/*
-Welcome the player 
-Ask player their name
-Create a player hit points variable and an npc hit points variable
-Create a Enemy opponents variable
-create a player barracks object
-What is a peon
-    Has a name, has a job (create peon function def job 'nothing')
-Define peon actions
-
-*/
 const prompt = require("prompt-sync")();
-const blankLine = () => console.log('')
-let playerHP = 10;
-let npcHP = 10;
-const playerCastle = {
-  peons: [],
-}; //where our peons go after creation
-let count = 0;
-//the below is npc actions
-const damagePlayer = () => {
-  let npcAttack = getRandomNumber(1, 5);
-  playerHP = playerHP - npcAttack;
-  console.log(
-    `Your foe attacks your base for ${npcAttack} damage. You have ${playerHP} remaining.`
-  );
-  if (playerHP <= 0) {
-    console.log(`The Enemy General has bested you in battle. Game Over! :(`);
-    gameStatus = false;
-  } else playerTurn = true;
-};
-const healNPC = () => {
-  let amtHealed = getRandomNumber(1, 5);
-  npcHP = npcHP + amtHealed;
-  console.log(
-    `The enemy General has built fortifications. They have gained ${amtHealed} HP and they now have ${npcHP} HP`
-  );
-  playerTurn = true;
-};
+const blankLine = () => console.log("");
 //The below function is from coreui.io and generates an integer in the specified range
-const getRandomNumber = (min,max) => {
-  min = Math.ceil(min) // Round up the minimum value
-  max = Math.floor(max) //Round Down the maximum Value
-return Math.floor(Math.random() * (max - min + 1)) + min //(chatgpt fixed code so that 0,1 would work as a range)
-}
+const getRandomNumber = (min, max) => {
+  min = Math.ceil(min); // Round up the minimum value
+  max = Math.floor(max); //Round Down the maximum Value
+  return Math.floor(Math.random() * (max - min + 1)) + min; //(chatgpt fixed code so that 0,1 would work as a range)
 
+  //player and npc HP Totals
+  let playerHP = 10;
+  let npcHP = 10;
+
+  //player castle and peons
+  const playerCastle = {
+    peons: [],
+  };
+
+  //peon count
+  let count = 0;
+
+  //NPC Actions
+  const damagePlayer = () => {
+    let npcAttack = getRandomNumber(1, 5);
+    playerHP = playerHP - npcAttack;
+    console.log(
+      `Your foe attacks your base for ${npcAttack} damage. You have ${playerHP} remaining.`
+    );
+    if (playerHP <= 0) {
+      console.log(`The Enemy General has bested you in battle. Game Over! :(`);
+      gameStatus = false;
+    } else playerTurn = true;
+  };
+  const healNPC = () => {
+    let amtHealed = getRandomNumber(1, 5);
+    npcHP = npcHP + amtHealed;
+    console.log(
+      `The enemy General has built fortifications. They have gained ${amtHealed} HP and they now have ${npcHP} HP`
+    );
+    playerTurn = true;
+  };
+};
 
 //the below is player actions
-const healPlayer = () => {
-  playerHP++;
-}
+const healPlayer = () => playerHP++;
 const damageNPC = () => npcHP--;
 
+//Create a peon for the player
 const createPeon = () => {
   const thisPeon = prompt(`What would you like to name peon # ${count + 1}?`);
   blankLine();
@@ -59,23 +53,19 @@ const createPeon = () => {
   let addPeon = { name: thisPeon, job: "no job" };
   playerCastle.peons.push(addPeon);
   count++;
-  console.log(`You currently have ${playerHP} HP and your `);
+  console.log(
+    `You currently have ${playerHP} HP and your for presently has ${npcHP} HP.`
+  );
   playerTurn = false;
 };
 
-/*
-The below function should do the following
-Offer the player the ability to assign the peon the job attack or repair
-After assignment check each peon role and execute appropriate action
-check npc health
-  if npc health = 0 display victory message
-  if not 0 become npc turn
-// */
+// Assign peons Jobs and Perform associated actions
 const assignPeons = () => {
   for (let i = 0; i < playerCastle.peons.length; i++) {
     const assignRole = prompt(
-      `What job would you like to assign ${playerCastle.peons[i].name}? Press 1 for "Attack" or 2 for "Fortify": `);
-      blankLine()
+      `What job would you like to assign ${playerCastle.peons[i].name}? Press 1 for "Attack" or 2 for "Fortify": `
+    );
+    blankLine();
     if (assignRole === "1") {
       playerCastle.peons[i].job = "Attack";
       damageNPC();
@@ -87,27 +77,26 @@ const assignPeons = () => {
   }
   if (npcHP <= 0) {
     console.log(`You did it General ${username}, you have won!`);
-    gameStatus = false;
+    gameStatus = false; //ends the game if applicable
   } else {
-    console.log(`You presently have ${playerHP} HP and your foe has ${npcHP}`)
-    blankLine()
+    console.log(`You presently have ${playerHP} HP and your foe has ${npcHP}`);
+    blankLine();
     playerTurn = false;
   }
 };
+//Game introduction and premise
+console.log(`Welcome to Castle Battle`);
+blankLine();
 
-
-console.log(`Welcome to Castle Battle`); //welcome message
-blankLine()
-//explanation
 console.log(
   "Castle Battle is a game in which you and an enemy npc will take turns either building up your forces, attacking each other or repairing your own castle."
 );
-blankLine()
+blankLine();
 //Get user's name
 const username = prompt("What is your name?: ");
 
 console.log(`Welcome General ${username}`);
-blankLine()
+blankLine();
 let gameStatus = true; //starts the game
 let playerTurn = true; //starts player turn
 
@@ -118,18 +107,22 @@ while (playerTurn === true && gameStatus === true) {
   );
   if (playerAction === "1") {
     createPeon();
+    blankLine();
   }
   if (playerAction === "2") {
     assignPeons();
+    blankLine();
   }
   // We define the npc turn below
   while (playerTurn === false && gameStatus === true) {
-    const npcAction = getRandomNumber(0,1)
+    const npcAction = getRandomNumber(0, 1);
     if (npcAction === 0) {
       damagePlayer();
+      blankLine();
     }
     if (npcAction === 1) {
       healNPC();
+      blankLine();
     }
   }
 }
